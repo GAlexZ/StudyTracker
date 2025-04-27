1fc912eefd93794cf633fc937ba3cd462e9c6b15
Alexander Zimmermann <azimmerm@student.ethz.ch>
2025-04-27 13:05:59 +0200

    Initial Commit

---
+++ StudyTracker/StudyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTrackerApp.swift
+//  StudyTracker
+//
+//  Created by Alexander Zimmermann on 27.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct StudyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
